c05f6ca62a9bf9ef20cb15797bb6c3839d67d520
Sodikcp <paramesh.rah@gmail.com>
2023-12-20 13:06:05 +0700

    feat:complete execise styling

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,91 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+export function Alert(props) {
+  if (props.status === "error") {
+    return (
+      <>
+        <div
+          css={css`
+            display: flex;
+            color: black;
+            width: 650px;
+            height: 76px;
+            background-color: #f9c8c8;
+            align-items: center;
+            padding-left: 30px;
+            gap: 15px;
+            border-radius: 5px;
+          `}
+        >
+          <img src="./src/image/frown.png"></img>
+          This is error alert box
+        </div>
+      </>
+    );
+  } else if (props.status === "warning") {
+    return (
+      <>
+        <div
+          css={css`
+            display: flex;
+            color: black;
+            width: 650px;
+            height: 76px;
+            background-color: #f9d9c8;
+            align-items: center;
+            padding-left: 30px;
+            gap: 15px;
+            border-radius: 5px;
+          `}
+        >
+          <img src="./src/image/alert-triangle.png"></img>
+          This is warning alert box
+        </div>
+      </>
+    );
+  } else if (props.status === "info") {
+    return (
+      <>
+        <div
+          css={css`
+            display: flex;
+            color: black;
+            width: 650px;
+            height: 76px;
+            background-color: #f9ebc8;
+            align-items: center;
+            padding-left: 30px;
+            gap: 15px;
+            border-radius: 5px;
+          `}
+        >
+          <img src="./src/image/alert-circle.png"></img>
+          This is info alert box
+        </div>
+      </>
+    );
+  } else if (props.status === "success") {
+    return (
+      <>
+        <div
+          css={css`
+            display: flex;
+            color: black;
+            width: 650px;
+            height: 76px;
+            background-color: #cef7cd;
+            align-items: center;
+            padding-left: 30px;
+            gap: 15px;
+            border-radius: 5px;
+          `}
+        >
+          <img src="./src/image/check-circle.png"></img>
+          This is success alert box
+        </div>
+      </>
+    );
+  }
+}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,47 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <button
+        css={css`
+          background-color: #074ee8;
+          color: white;
+          width: 171.19px;
+          height: 50px;
+        `}
+      >
+        Button
+      </button>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <button
+        css={css`
+          background-color: #07a4e8;
+          color: white;
+          width: 171.19px;
+          height: 50px;
+        `}
+      >
+        Button
+      </button>
+    );
+  }
+  return (
+    <>
+      <button
+        css={css`
+          color: white;
+          width: 200px;
+          height: 50px;
+          background-color: black;
+        `}
+      >
+        Button
+      </button>
+    </>
+  );
+}
